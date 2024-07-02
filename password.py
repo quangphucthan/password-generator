2a5b6a4dda5c2399ab5590afa8d2147b6035667d
Phuc Than <quangphucthan@gmail.com>
2024-07-02 17:45:15 -0300

    User inputs and display the desired password

--- password.py
+++ password.py
@@ -7,4 +7,9 @@ def generate_password(minlength, numbers=True, specialChars=True):
     nums = string.digits
     special = string.punctuation
 
-    
+minInput = int(input("Enter the minimum length of the password: "))
+hasNumber = input("Do you want numbers in your password? (y/n): ").lower() == "y"
+hasSpecial = input("Do you want special characters in your password? (y/n): ").lower() == "y"
+
+pwd = generate_password(minInput, hasNumber, hasSpecial)
+print(pwd)
