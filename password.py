95ac76f23423de21291fc6ec3fe60d53970970e2
Phuc Than <quangphucthan@gmail.com>
2024-07-02 17:58:24 -0300

    Adding logics to generate password

--- password.py
+++ password.py
@@ -7,6 +7,36 @@ def generate_password(minlength, numbers=True, specialChars=True):
     nums = string.digits
     special = string.punctuation
 
+    characters = letters
+    if numbers:
+        characters += nums
+    if specialChars:
+        characters += special
+
+    password = ""
+    checker = False
+    hasNumber = False
+    hasSpecial = False
+
+    while not checker or len(password) < minlength:
+        
+        pwd = random.choice(characters)
+        password += pwd
+
+        if pwd in nums:
+            hasNumber = True
+        elif pwd in special:
+            hasSpecial = True
+
+        checker = True
+
+        if numbers:
+            checker = hasNumber
+        if specialChars:
+            checker = checker and hasSpecial
+
+    return password
+
 minInput = int(input("Enter the minimum length of the password: "))
 hasNumber = input("Do you want numbers in your password? (y/n): ").lower() == "y"
 hasSpecial = input("Do you want special characters in your password? (y/n): ").lower() == "y"
